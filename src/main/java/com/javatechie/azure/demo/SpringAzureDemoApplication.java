4f3cdbff2cc55d59e42ec37609f5e6cbceabbe00
Devenara Kumar <it.admin@elixirct.com>
2023-03-24 15:12:56 +0530

    this is my second commit

--- src/main/java/com/javatechie/azure/demo/SpringAzureDemoApplication.java
+++ src/main/java/com/javatechie/azure/demo/SpringAzureDemoApplication.java
@@ -11,7 +11,7 @@ public class SpringAzureDemoApplication {
 
 	@GetMapping("/message")
 	public String message(){
-		return "Congrats ! your application deployed successfully in Azure Platform. this is my First application !";
+		return "This is my second commit. !";
 	}
 
 	public static void main(String[] args) {
